b47b37102074d3886995b71788734e6c74c0d42c
jeff231li <pea231@gmail.com>
2020-05-09 17:03:43 -0700

    Add a utility function that reads restraint definition from an ASCII file

--- paprika/restraints/utils.py
+++ paprika/restraints/utils.py
@@ -26,3 +26,54 @@ def parse_window(window):
     window = int(window[1:])
 
     return window, phase
+
+
+def read_restraint_from_file(filename):
+    """
+    Simple function to read in restraints from an ASCII template file
+
+    Parameters
+    ----------
+    filename : str
+        file name of template file
+
+    Returns
+    -------
+    restraints : dict
+        dictionary of restraints containing information of the atoms, target and spring constant
+
+    Examples
+    --------
+    ASCII file should contain the atoms (2, 3 or 4), the equilibrium target and spring constant
+
+        :1@C12 :2@C4  12.5 50.0
+        :1@O11 :1@C12 :1@O13 :1@C14  115.47 6.0
+        :1@C12 :1@O13 :1@C14 :1@C15 -121.16 6.0
+
+    """
+    restraints = {'atoms': [], 'target': [], 'k': []}
+
+    with open(filename, 'r') as file:
+        for line in file:
+            if not line.startswith("#"):
+                restr = line.split()
+
+                # Distance
+                if len(restr) == 4:
+                    restraints['atoms'].append([restr[0], restr[1]])
+                    restraints['target'].append(float(restr[2]))
+                    restraints['k'].append(float(restr[3]))
+
+                # Angle
+                elif len(restr) == 5:
+                    restraints['atoms'].append([restr[0], restr[1], restr[2]])
+                    restraints['target'].append(float(restr[3]))
+                    restraints['k'].append(float(restr[4]))
+
+                # Dihedral
+                elif len(restr) == 6:
+                    restraints['atoms'].append([restr[0], restr[1], restr[2], restr[3]])
+                    restraints['target'].append(float(restr[4]))
+                    restraints['k'].append(float(restr[5]))
+
+    return restraints
