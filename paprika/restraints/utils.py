8d9cbc5a9642638a50f1cd3b7a2aaf3e1ea1a775
jeff231li <pea231@gmail.com>
2020-05-31 18:10:54 -0700

    changed utils.py

--- paprika/restraints/utils.py
+++ paprika/restraints/utils.py
@@ -28,53 +28,56 @@ def parse_window(window):
     return window, phase
 
 
-def read_restraint_from_file(filename):
+def restraints_from_ascii(filename):
     """
-    Simple function to read in restraints from an ASCII template file
+    Utility function to read in restraints from a simple ASCII file.
 
     Parameters
     ----------
     filename : str
-        file name of template file
+        file name of template file.
 
     Returns
     -------
     restraints : dict
-        dictionary of restraints containing information of the atoms, target and spring constant
+        dictionary of restraints containing information of the atoms, target and spring constant.
 
     Examples
     --------
-    ASCII file should contain the atoms (2, 3 or 4), the equilibrium target and spring constant
+    ASCII file should contain the atoms (2, 3 or 4), the equilibrium target and spring constant.
 
-        :1@C12 :2@C4  12.5 50.0
-        :1@O11 :1@C12 :1@O13 :1@C14  115.47 6.0
+        :1@C12 :2@C4  12.5 5.0
+        :1@O11 :1@C2 :1@C3  90.0 50.0
         :1@C12 :1@O13 :1@C14 :1@C15 -121.16 6.0
 
     """
-    restraints = {'atoms': [], 'target': [], 'k': []}
+    restraints = {'atoms': [], 'target': [], 'k': [], 'type': []}
 
     with open(filename, 'r') as file:
         for line in file:
             if not line.startswith("#"):
                 line = line.split()
 
-                # Distance
                 if len(line) == 4:
                     restraints['atoms'].append([line[0], line[1]])
                     restraints['target'].append(float(line[2]))
                     restraints['k'].append(float(line[3]))
+                    restraints['type'].append('bond')
 
-                # Angle
                 elif len(line) == 5:
                     restraints['atoms'].append([line[0], line[1], line[2]])
                     restraints['target'].append(float(line[3]))
                     restraints['k'].append(float(line[4]))
+                    restraints['type'].append('angle')
 
-                # Dihedral
                 elif len(line) == 6:
                     restraints['atoms'].append([line[0], line[1], line[2], line[3]])
                     restraints['target'].append(float(line[4]))
                     restraints['k'].append(float(line[5]))
+                    restraints['type'].append('dihedral')
+
+                else:
+                    print("Restraint given is not a bond, angle or dihedral... skipping line.")
 
     return restraints
 
